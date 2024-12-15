efde3642a86b80ab0fbc792d0e81c114a250817c
Umberto Amenta <umberto.amenta@hotmail.it>
2024-12-15 19:52:32 +0100

    fix: +img_alt-logic

--- js/script.js
+++ js/script.js
@@ -10,9 +10,11 @@ const bulbBtnDom = document.querySelector("#bulb > button")
 bulbBtnDom.addEventListener("click", function() {
         if (bulbDom.src.includes("img/white_lamp.png")) {
             bulbDom.src = "img/yellow_lamp.png"
+            bulbDom.alt = "lampadina accesa"
             bulbBtnDom.innerHTML = "Spegni"
         } else if (bulbDom.src.includes("img/yellow_lamp.png")) {
             bulbDom.src = "img/white_lamp.png"
+            bulbDom.alt = "lampadina spenta"
             bulbBtnDom.innerHTML = "Accendi"
         }
         
